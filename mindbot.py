e90a0b0d535548f2f2c8d258d22639c468b52ac5
Romain Lapostolle <rlapostolle@gmail.com>
2022-11-04 22:32:07 +0100

    first command

---
+++ mindbot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import discord
+from discord import app_commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "card", description = "Display a card by name")
+async def card(interaction: discord.Interaction, card: str):
+                name=card.replace(' ', '_')
+                await interaction.response.send_message(f'https://mindbug.fandom.com/wiki/{name}')
+
+@client.event
+async def on_ready():
+	#use a specific guild as Mindbug France: 1010510391878619146, sync is faster in that case.
+    await tree.sync(guild=discord.Object(id= 1010510391878619146))
+    print("Ready!")
+
+client.run(File.readlines("token.txt"))
