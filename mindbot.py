a900d8f14f62e483bbf2322e1933a1fbf4bdeb6a
Romain Lapostolle <rlapostolle@gmail.com>
2022-11-04 22:46:25 +0100

    fix

--- mindbot.py
+++ mindbot.py
@@ -8,16 +8,16 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "card", description = "Display a card by name", guild=discord.Object(id= 1010510391878619146))
+@tree.command(name = "card", description = "Display a card by name")
 async def card(interaction: discord.Interaction, card: str):
-                name=card.replace(' ', '_')
-                await interaction.response.send_message(f'https://mindbug.fandom.com/wiki/{name}')
+	name=card.replace(' ', '_')
+	await interaction.response.send_message(f'https://mindbug.fandom.com/wiki/{name}')
 
 @client.event
 async def on_ready():
 	#use a specific guild as Mindbug France: 1010510391878619146, sync is faster in that case.
-    await tree.sync(guild=discord.Object(id= 1010510391878619146))
-    print("Ready!")
+	await tree.sync(guild=discord.Object(id= 1010510391878619146))
+	print("Ready!")
 
 with open('token.txt') as f:
 	token=f.read()
