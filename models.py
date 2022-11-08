8f8b6ce88fb38ff176dd4c32ad9fc50e2acd0e92
TheMBeat <thembeat@gmx.de>
2022-11-08 23:20:36 +0100

    CreateCardCommands (#3)
    * CreateCardCommands
    
    * Add missing import and missing param
    
    Co-authored-by: Romain Lapostolle <rlapostolle@gmail.com>

---
+++ models.py
@@ -0,0 +1,17 @@
+# This needs to be redesigned, it is only for the first test
+class Card():
+    def __init__(self, uid_from_set, lang, name, power, keywords, effect, quote, image_path, filename, cardset = ""): 
+        self.uid_from_set = uid_from_set # 0
+        self.lang = lang # 1
+        self.name = name # 3
+        self.power = power
+        self.keywords = keywords
+        self.effect = effect
+        self.quote = quote
+        self.image_path = image_path # 6
+        self.filename = filename # from image_path
+        self.cardset = cardset #8
+        self.cropped_final_card_base64 = ""
+        self.final_card_base_64 = ""
+        self.artwork_base64 = ""
+        self.set_icon_base64 = ""
